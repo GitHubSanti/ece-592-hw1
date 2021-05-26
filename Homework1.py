9b26633bc53a4cf10cc5542fa18ef016d12dbcd6
Santiago Sepulveda <sasepulv@ncsu.edu>
2021-05-26 13:22:15 -0400

    use of lambdas

--- Homework1.py
+++ Homework1.py
@@ -1,3 +1,13 @@
+"""
+@author Santiago Sepulveda
+
+Following script was created with the four functions:
+pyramid(s)
+findSquares(s:int = 0, e:int = 0)
+calSalary(hours,r)
+calLetterGrade(points,gradescale)
+
+"""
 import math
 
 def pyramid(s):
@@ -11,7 +21,8 @@ def pyramid(s):
 
 def findSquares(s:int = 0, e:int = 0):
         """          
-        Print numbers which are exact squares of an integer within range of two integers
+        Return list of squares. Print numbers which are exact squares of an integer within range of two integers. 
+        s is starting integer, e is ending integer.
         """
         squares = []
         if s > e:
@@ -31,10 +42,20 @@ def findSquares(s:int = 0, e:int = 0):
         print(squares)
         return squares
 
-def calSalary(hours,r):
+def calSalary(h:float,r:float = 20):
         """
+        Return the salary in float. h is number of hours and r is the hourly rate.
 
         """
+        if h < 0:
+                print("Not valid Hours")
+                return -1
+        elif h >= 0 and h <= 40:
+                salary = lambda h,r : h*r
+                return salary
+        else:
+                salary = lambda h,r : 40*r + (h-40)*(r*1.2)
+                return salary
 
 def calLetterGrade(points,gradescale):
         """
